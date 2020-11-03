042b0d960c0017c5226a47a34d5f4cd40be7bb08
Phil Wang <lucidrains@gmail.com>
2020-11-02 19:43:47 -0800

    fix a bug and add comments to memory replay back prop

--- memformer/mrbp.py
+++ memformer/mrbp.py
@@ -10,18 +10,25 @@ def memory_replay_backprop(
 ):
     b, *_ = src.shape
 
+    # get initial memory and max sequence length from encoder
     mem_init = model.get_initial_mem(b)
     max_seq_len = model.encoder.max_seq_len
 
+    # instantiate memory replay buffer
     replay_buffer = [mem_init]
 
+    # split sequences and masks
     src_segs = src.split(max_seq_len, dim = 1)
     num_segs = len(src_segs)
     src_mask_segs = src_mask.split(max_seq_len, dim = 1) if src_mask is not None else ((None,) * num_segs)
 
+    # for now, assume target sequence and mask is passed at the very last segment
+    # todo - allow to tether a target sequence at any point in the segment
+    #        and attach custom loss to encoder output
     tgt_segs = ((None,) * (num_segs - 1)) + (tgt,)
     tgt_mask_segs = ((None,) * (num_segs - 1)) + (tgt_mask,)
 
+    # run forwards and gather all memories
     prev_mem = mem_init
     with torch.no_grad():
         for i in range(num_segs - 1):
@@ -30,9 +37,16 @@ def memory_replay_backprop(
             replay_buffer.append(mem)
             prev_mem = mem
 
+    # do backpropagation one segment at a time from last step to first
     mem_grad = torch.zeros_like(prev_mem)
     for i in reversed(range(num_segs)):
         src, src_mask, tgt, tgt_mask, mems = map(itemgetter(i), (src_segs, src_mask_segs, tgt_segs, tgt_mask_segs, replay_buffer))
-        _, mem, tgt_loss = model(src = src, tgt = tgt, src_mask = src_mask, tgt_mask = tgt_mask)
+        mems = mems.requires_grad_()
+
+        _, mems_next, tgt_loss = model(src = src, tgt = tgt, src_mask = src_mask, tgt_mask = tgt_mask, mems = mems)
         tgt_loss.backward(retain_graph = True)
-        mem.backward(mem_grad, retain_graph = True)
+        mems_next.backward(mem_grad, retain_graph = True)
+
+        # if not the last step, pass the next memory's gradient back a step
+        if i != 0:
+            mem_grad.copy_(mems.grad.data)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'memformer',
   packages = find_packages(exclude=['examples']),
-  version = '0.2.0',
+  version = '0.2.1',
   license='MIT',
   description = 'Memformer - Pytorch',
   author = 'Phil Wang',
