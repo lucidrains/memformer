332531e529a1b7098fc13a713d7f971489d09621
Phil Wang <lucidrains@gmail.com>
2020-11-02 19:29:03 -0800

    first pass at memory-replay backpropagation

--- memformer/__init__.py
+++ memformer/__init__.py
@@ -1,1 +1,2 @@
 from memformer.memformer import Memformer
+from memformer.mrbp import memory_replay_backprop
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'memformer',
   packages = find_packages(exclude=['examples']),
-  version = '0.1.1',
+  version = '0.2.0',
   license='MIT',
   description = 'Memformer - Pytorch',
   author = 'Phil Wang',
